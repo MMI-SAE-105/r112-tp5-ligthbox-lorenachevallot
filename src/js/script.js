4b0902c4716333798ab91595567ee3d78c150684
lorenachevallot <loni.chevallot@gmail.com>
2024-12-04 08:33:15 +0100

    Ajout const image

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,6 @@
 
 // Récupère l'élément HTML avec l'ID "lightbox"
 const lightBox = document.querySelector("#lightbox");
+
+// Cible l'élément <img> contenu dans l'élément avec l'ID "lightbox"
+const image = document.querySelector("lightbox img");
