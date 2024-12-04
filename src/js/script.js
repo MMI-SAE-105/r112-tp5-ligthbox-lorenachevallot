f5bd2acf1050745a6376850a131a4a12fe5f3e52
lorenachevallot <loni.chevallot@gmail.com>
2024-12-04 08:38:17 +0100

    ajout écouteur d'événements (detecte clics)

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,6 @@ const lightBox = document.querySelector("#lightbox");
 
 // Cible l'élément <img> contenu dans l'élément avec l'ID "lightbox"
 const image = document.querySelector("#lightbox img");
+
+// Ajoute un écouteur d'événements pour détecter les clics sur tout le doc
+document.addEventListener("click", function (e) {
